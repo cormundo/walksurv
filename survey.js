9d510302caabd47a056fc82fe4e9090e6322cf51
cormundo <37988060+cormundo@users.noreply.github.com>
2021-03-28 15:11:56 +0300

    Update survey.js

--- survey.js
+++ survey.js
@@ -37,7 +37,7 @@ function checkCookieCount() {
   if (userCNT != "") {
     alert("Your Previous Count: " + userCNT);
     // make global count update
-    count = userCNT
+    count = userCNT;
   } else {
     kount = count;
     if (kount != "" && kount != null) {
