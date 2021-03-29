6fcf01110bc856959842969ec12618b382da1354
cormundo <37988060+cormundo@users.noreply.github.com>
2021-03-29 08:59:28 +0300

    Add files via upload

--- survey.js
+++ survey.js
@@ -50,26 +50,14 @@ function checkCookieCount() {
   }
 }
 
-//end cookie section
-
-//cookie stuff over
-
-var knlist = keyNameList;
-
-var modal = document.getElementById('id01');
-
-var username;
-
 function start() {
   username = document.getElementById("user_PID").value;
   alert("your PID is " + username);
 
   checkCookieName();
   checkCookieCount();
-  
-  var oneMin = 60 * 60,
-      display = document.querySelector('#time');
-  startTimer(oneMin, display);
+
+  run_clock('clockdiv',deadline);
 
   jsoner();
 
@@ -84,43 +72,77 @@ function start() {
   document.getElementById("btn1").style = "display:none";
 };
 
-function startTimer(duration, display) {
 
-    var minTimer = duration, minutes, seconds;
-
-    setInterval(function () {
-        minutes = parseInt(minTimer / 60)
-        seconds = parseInt(minTimer % 60);
-
-        seconds = seconds < 10 ? "0" + seconds : seconds;
-
-        display.textContent = minutes + ":" + seconds;
-
-        if (--minTimer < 0) {
-    visualTimer.classList.remove("height-change");
-    minTimer = duration
+function time_remaining(endtime){
+	var t = Date.parse(endtime) - Date.parse(new Date());
+	var seconds = Math.floor( (t/1000) % 60 );
+	var minutes = Math.floor( (t/1000/60) % 60 );
+	var hours = Math.floor( (t/(1000*60*60)) % 24 );
+	var days = Math.floor( t/(1000*60*60*24) );
+	return {'total':t, 'days':days, 'hours':hours, 'minutes':minutes, 'seconds':seconds};
 }
 
-else if (minTimer == 0) {
-
-  alert("You have completed the exercise. Thank you for your participation.")
-  document.getElementById("flexo").style = "display:none";
-  document.getElementById("final").style = "display:visible";
+var timeinterval;
+function run_clock(id,endtime){
+	var clock = document.getElementById(id);
+	function update_clock(){
+		var t = time_remaining(endtime);
+		clock.innerHTML = 'Time Remaining: '+t.minutes+':'+t.seconds;
+		if(t.total<=0){
+      clearInterval(timeinterval);
+      alert("You have completed the exercise. Thank you for your participation.");
+      document.getElementById("flexo").style = "display:none";
+      document.getElementById("final").style = "display:visible";
+    }
+	}
+	update_clock(); // run function once at first to avoid delay
+	timeinterval = setInterval(update_clock,1000);
 }
 
-    }, 1000);
+
+var paused = false; // is the clock paused?
+var time_left; // time left on the clock when paused
+
+function pause_clock(){
+	if(!paused){
+		paused = true;
+		clearInterval(timeinterval); // stop the clock
+		time_left = time_remaining(deadline).total; // preserve remaining time
+    document.getElementById('pause').style = "background-color:#B31A39; padding:10px; font-size:15px";
+     document.getElementById("pause").innerHTML = "TIMER IS PAUSED";
+    document.getElementById("kounter1").style = "display:flex;width:100%;justify:left;background-color: #B31A39";
+    document.getElementById("kounter2").style = "display:flex;width:100%;justify:right;background-color: #B31A39";
+    document.getElementById('resume').style = "background-color:#70CFBC; padding:10px; font-size:15px; display:visible"
+	}
 }
 
+function resume_clock(){
+	if(paused){
+		paused = false;
 
+		// update the deadline to preserve the amount of time remaining
+		deadline = new Date(Date.parse(new Date()) + time_left);
+
+		// start the clock
+		run_clock('clockdiv',deadline);
+
+    document.getElementById('pause').style = "background-color:#1ab394; padding:10px; font-size:15px";
+    document.getElementById('resume').style = "background-color:#1ab394; padding:10px; font-size:15px; display:none";
+    document.getElementById("pause").innerHTML = "PAUSE TIMER";
+    document.getElementById("kounter1").style = "display:flex;width:100%;justify:left;background-color: #1ab394";
+    document.getElementById("kounter2").style = "display:flex;width:100%;justify:right;background-color: #1ab394";
+	}
+}
+
+// handle pause and resume button clicks
+document.getElementById('pause').onclick = pause_clock;
+document.getElementById('resume').onclick = resume_clock;
+
+
+///
 Survey
   .StylesManager
   .applyTheme("modern");
-
-var knlist_hold =  new Array();
-
-var count = 1;
-
-var knx;
 
 function knxer() {
   if (window.count === 1) {
@@ -132,15 +154,10 @@ function knxer() {
   }
 };
 
-var keyname;
-
-var mapilink;
-
-var catched = "false";
-
 function keynamer() {
   keyname = (knlist[knxer()]);
   keyN2 = keyname;
+  PID2 = document.getElementById("user_PID").value;
 }
 
 
@@ -150,13 +167,8 @@ function mapilinker() {
 
 function imgur() {
   document.getElementById('streetscape').src = mapilinker();
-  console.log(mapilink);
 }
 
-function usernamer() {
-  PID2 = document.getElementById("user_PID").value;
-  console.log(PID2);
-}
 
 function catcher() {
   if (((username*1) + (count*1)) % 9 === 0){
@@ -166,14 +178,11 @@ function catcher() {
   }
 }
 
-var json;
-
 function jsoner() {
   knxer();
   keynamer();
   mapilinker();
   imgur();
-  usernamer();
   catcher();
   console.log(catched);
 
@@ -280,12 +289,7 @@ function jsoner() {
 
       survey.sendResult('3cc0bc85-2253-412d-a56c-cc7b82260de3');
       update();
-      console.log(knx);
-      console.log(count);
-      console.log(keyname);
-      console.log(mapilink);
-      console.log(catched);
-      console.log(username);
+
       survey.clear();
       survey.render();
     });
@@ -298,11 +302,34 @@ function jsoner() {
 
 function update() {
   setCookie("userC", count, 365);
-  knxer();
-  keynamer();
-  mapilinker();
-  imgur();
-  usernamer();
   jsoner();
   document.getElementById("kounter2").innerHTML = "Images Tagged: " + (count - 1);
 }
+
+var cookieCNT;
+
+var knlist_hold =  new Array();
+
+var count = 1;
+
+var knx;
+
+var knlist = keyNameList;
+
+var modal = document.getElementById('id01');
+
+var username;
+
+var keyname;
+
+var mapilink;
+
+var catched = "false";
+
+var json;
+
+var time_in_minutes = 60;
+
+var current_time = Date.parse(new Date());
+
+var deadline = new Date(current_time + time_in_minutes*60*1000);
